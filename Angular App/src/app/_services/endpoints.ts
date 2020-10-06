76d10f66dfbb449e13b9c15ddc518b689f6bc9f1
Saba Fathima <sabafathima1704@gmail.com>
2020-10-06 23:27:44 +0200

    adding angular project

---
+++ Angular App/src/app/_services/endpoints.ts
@@ -0,0 +1,13 @@
+export const endpoints = {
+  // Change this URL if you want to point the React application at another Pega server.
+  BASEURL: "https://cognizante2efs83.pegatsdemo.com/prweb/api/v1",
+  AUTH: "/authenticate",
+  CASES: "/cases",
+  CASETYPES: "/casetypes",
+  VIEWS: "/views",
+  ASSIGNMENTS: "/assignments",
+  ACTIONS: "/actions",
+  PAGES: "/pages",
+  DATA: "/data",
+  REFRESH: "/refresh"
+};
